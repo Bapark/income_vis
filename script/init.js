556e07bdcfeb116fd9c4a9fb69703cd96f21ed84
Brian Park <ba_park@hotmail.com>
2018-11-09 14:55:23 -0700

    add data setup

---
+++ script/init.js
@@ -0,0 +1,37 @@
+
+
+d3.csv("data/H-8Median_household_Income_2017adjusted.csv").then(incomedata => {
+    console.log(incomedata);
+   
+});
+
+
+//setup racial data sets 
+
+
+async function loadRaceData(){
+	let allRaces =  await d3.csv("data/h01AR.csv");
+
+	let asiandata = await  d3.csv("data/h01A.csv");
+
+	let blackdata = await  d3.csv("data/h01B.csv");
+
+	let hispdata = await  d3.csv("data/h01H.csv");
+
+	let whitedata = await  d3.csv("data/h01WNH.csv");
+
+	let raceData = {
+		AllRace : allRaces,
+		Asian : asiandata,
+		Black : blackdata,
+		Hispanic : hispdata,
+		White : whitedata
+	}
+
+	console.log(raceData);
+
+}
+
+loadRaceData();
+
+//TODO construct view
