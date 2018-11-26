b10f30fee82f97fadc2be9a53ca7c70c2c9db5ff
jarednielson <jarednielsonn@gmail.com>
2018-11-26 03:41:44 -0700

    Finish bar charts

--- script/aggregatechart.js
+++ script/aggregatechart.js
@@ -3,30 +3,41 @@
  */
 class AggregateIncomeBarPlot {
     constructor(data) {
-        //console.log(data);
         this.margin = { top: 20, right: 20, bottom: 60, left: 80 };
         this.width = 875 - this.margin.left - this.margin.right;
         this.height = 500 - this.margin.top - this.margin.bottom;
         this.data = data;
+        this.reducedData = {};
+        for(let race in this.data) {
+            this.reducedData[race] = this.data[race].reduce((accum, curr) => {
+                for(let category in curr){
+                    if(category == 'year' || category == 'number'){
+                        continue;
+                    }
+                    accum[category] += curr[category];
+                }
+                return accum;
+            });
+        }
 
         this.colorScales = {};
-
-        this.colorScales.overall = d3.scaleLinear().domain([1,5])
+        this.colorScales.overall = d3.scaleLinear().domain([1,6])
                                     .range([d3.rgb('#000000'), d3.rgb('#d3d3d3')]);
-        this.colorScales.white = d3.scaleLinear().domain([1,5])
+        this.colorScales.white = d3.scaleLinear().domain([1,6])
                                     .range([d3.rgb("#E51A00"), d3.rgb('#EECCC3')]);
-        this.colorScales.black = d3.scaleLinear().domain([1,5])
+        this.colorScales.black = d3.scaleLinear().domain([1,6])
                                     .range([d3.rgb("#0B3AE5"), d3.rgb('#ACBBEC')]);
-        this.colorScales.asian = d3.scaleLinear().domain([1,5])
+        this.colorScales.asian = d3.scaleLinear().domain([1,6])
                                     .range([d3.rgb("#00A80F"), d3.rgb('#D4F4D2')]);
-        this.colorScales.hispanic = d3.scaleLinear().domain([1,5])
+        this.colorScales.hispanic = d3.scaleLinear().domain([1,6])
                                     .range([d3.rgb("#7000A8"), d3.rgb('#EFDBF5')]);
 
         this.colorScales.top5 = 1;
-        this.colorScales.fourth = 2;
-        this.colorScales.third = 3;
-        this.colorScales.second = 4;
-        this.colorScales.lowest = 5;
+        this.colorScales.highest = 2;
+        this.colorScales.fourth = 3;
+        this.colorScales.third = 4;
+        this.colorScales.second = 5;
+        this.colorScales.lowest = 6;
 
         this.drawPlot();
     }
@@ -64,17 +75,17 @@ class AggregateIncomeBarPlot {
             .domain(['Temp1', 'Temp2'])
             .range([0, this.width]);
         this.yScale = d3.scaleLinear()
-            .domain([0, 300000])
+            .domain([0, 1600])
             .range([this.height, 0])
             .nice();
 
-        let xAxis = d3.axisBottom();
-        let yAxis = d3.axisLeft();
-        xAxis.scale(this.xScale);
-        yAxis.scale(this.yScale)
+        this.xAxis = d3.axisBottom();
+        this.yAxis = d3.axisLeft();
+        this.xAxis.scale(this.xScale);
+        this.yAxis.scale(this.yScale)
             .ticks(10);
-        d3.select('#x-axis-aggregatechart').call(xAxis);
-        d3.select('#y-axis-aggregatechart').call(yAxis);
+        d3.select('#x-axis-aggregatechart').call(this.xAxis);
+        d3.select('#y-axis-aggregatechart').call(this.yAxis);
         
         this.barGroup = this.svg.append('g')
                     .attr('id', 'bar-group-aggregatechart');
@@ -90,38 +101,42 @@ class AggregateIncomeBarPlot {
         let checked = document.querySelectorAll('input.sub-button:checked');
 
         let nextData = [];
+        let bands = [];
 
         checked.forEach((elem) => {
             let arr = elem.id.split('-');
-
+            bands.push(`${arr[0].toUpperCase()} ${arr[1].toUpperCase()}`);
+            
             let d = {};
-            d.data = that.data[arr[0]].map(elem => {
-                return { 
-                            'value': parseInt(elem[arr[1]]),
-                            'year' : parseInt(elem.year)
-                        };
-            });
+            d.value = this.reducedData[arr[0]][arr[1]];
             d.category = arr[0];
             d.pentile = arr[1];
             nextData.push(d);
         });
-        let paths = this.barGroup.selectAll('path')
+
+        this.xScale = d3.scaleBand()
+        .domain(bands)
+        .range([0, this.width])
+        .paddingInner(0.05);
+        this.xAxis = d3.axisBottom().scale(this.xScale);
+        d3.select('#x-axis-aggregatechart').call(this.xAxis);
+        
+        let bars = this.barGroup.selectAll('rect')
                             .data(nextData);
 
-        let pathsEnter = paths.enter().append('path');
-        paths.exit().remove();
-        paths = paths.merge(pathsEnter);
+        let barsEnter = bars.enter().append('rect');
+        bars.exit().remove();
+        bars = bars.merge(barsEnter);
 
-        let lineFn = d3.line()
-                        .x((d) => that.xScale(new Date(d.year, 0, 1, 0)) + that.margin.left)
-                        .y((d) => that.yScale(d.value) + that.margin.top);
-        paths.attr('d', (d) => lineFn(d.data))
-            .attr('stroke', (d) => {
+
+        bars.attr('fill', (d) => {
                 return that.colorScales[d.category](that.colorScales[d.pentile])}) //TODO add color scales
-            .attr('stroke-width', 2)
-            .attr('fill', 'none');
-        
-       
+            .attr('width', this.xScale.bandwidth())
+            .attr('height', (d) => this.height - this.yScale(d.value))
+            .attr('x', (d) => this.xScale(`${d.category.toUpperCase()} ${d.pentile.toUpperCase()}`))
+            .attr('y', (d) => this.yScale(d.value));
+        this.barGroup
+            .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
     }
 
     drawPath(path, color) {
