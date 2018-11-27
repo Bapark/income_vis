f0e5c39292507377efa4c7c0eb59722202fae635
Brian Park <ba_park@hotmail.com>
2018-11-27 13:31:15 -0700

    almost setup donut, need to get it working with svg or swap to canvas

--- script/wealthchart.js
+++ script/wealthchart.js
@@ -10,19 +10,37 @@ class WealthChart {
 	}
 
 	drawChart(){
-		let pie = d3.layout.pie()
-						.value((d)=>{}) //TODO set correct data
+
+		let sliderVal = document.getElementById("slider").value;
+		let defData = this.wealthData.filter( (d)=>{return d.year == sliderVal});
+
+		let dataArray = [
+	    	{ name: "90-95", percent: defData[0]["90-95"] },
+	    	{ name: "95-99", percent: defData[0]["95-99"]  },
+	    	{ name: "99-99.5", percent: defData[0]["99-99.5"]  },
+	    	{ name: "99.5-99.9", percent: defData[0]["99.5-99.9"]  },
+	    	{ name: "99.9-99.99", percent: defData[0]["99.9-99.99"]  },
+	    	{ name: "bottom_90", percent: defData[0].bottom_90  }
+		];
+
+
+
+		let pie = d3.pie()
+						.value((d)=>{return d.percent}) //TODO set correct data
 						.sort(null)
 						.padAngle(.02);
 
 		let svg = d3.select('#wealthChart').append('svg')
-			.attr({width:this.width, height:this.height})
-			.append('g').attr({transform:'translate('+ this.width/2 +',' + this.height/2 +')'});
+			.attr('width',this.width)
+			.attr('height',this.height)
+			.append('g').attr('transform','translate('+ this.width/2 +',' + this.height/2 +')');
 
 		let outerRad = this.width/2;
 		let innerRad = 120;
 
 		let arc = d3.svg.arc().outerRadius(outerRad).innerRadius(innerRad);
+
+		let arcs = pie(dataArray);
 
 		let path = svg.selectAll('path')
 			.data(pie(this.data))
