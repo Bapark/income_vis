069071a00bbb7a3cb72e6b9fcdebeb7c6e6da335
Brian Park <ba_park@hotmail.com>
2018-11-26 23:52:43 -0700

    setup donut

--- script/wealthchart.js
+++ script/wealthchart.js
@@ -5,14 +5,45 @@ class WealthChart {
         this.width = 875 - this.margin.left - this.margin.right;
         this.height = 500 - this.margin.top - this.margin.bottom;
 
-
-
+        this.drawChart();
 
 	}
 
+	drawChart(){
+		let pie = d3.layout.pie()
+						.value((d)=>{}) //TODO set correct data
+						.sort(null)
+						.padAngle(.02);
+
+		let svg = d3.select('#wealthChart').append('svg')
+			.attr({width:this.width, height:this.height})
+			.append('g').attr({transform:'translate('+ this.width/2 +',' + this.height/2 +')'});
+
+		let outerRad = this.width/2;
+		let innerRad = 120;
+
+		let arc = d3.svg.arc().outerRadius(outerRad).innerRadius(innerRad);
+
+		let path = svg.selectAll('path')
+			.data(pie(this.data))
+			.enter()
+			.append('path')
+			.attr({
+				d:arc,
+				fill: function(d,i){
+					return color(d.data.name); //TODO ensure this is correct
+				}
+			});
 
 
 
+		//todo set transition
+
+	}
+
+	updateChart(){
+
+	}
 
 
 }
